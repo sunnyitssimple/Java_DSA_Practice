c06c80f56c4f0c77f5ecae86bbcbe2e6a1d0a52e
sunnyitssimple <sunnyshambhu620201@gmail.com>
2025-06-01 08:49:53 +0530

    vid 8  done

--- Basics/P2.java
+++ Basics/P2.java
@@ -1,5 +1,12 @@
+
+import java.util.Scanner;
+
 public class P2 {
     public static void main(String[] args) {
-        int a=10;
+        System.out.println("Enter the amount of rupee required");
+        Scanner Amount =new Scanner(System.in);
+        int Value=Amount.nextInt();
+        System.out.println("The value for money  is "+Value);
+        Amount.close();
     }
 }
---
+++ Basics/P3.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class P3 {
+    public static void main(String[] args) {
+        Scanner num=new Scanner(System.in);
+         int num1=num.nextInt();
+         int num2=num.nextInt();
+         int Sum=num1+num2;
+         System.out.println("The sum is "+Sum);
+         num.close();
+         //typecasting
+         int a=(int)(67.45f);
+         System.out.println(a);
+    }
+}
