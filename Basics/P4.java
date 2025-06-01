2f142f90a945b3e1179ea2fb0548dd8001e70d82
sunnyitssimple <sunnyshambhu620201@gmail.com>
2025-06-01 17:49:41 +0530

    video 9 done

---
+++ Basics/P4.java
@@ -0,0 +1,25 @@
+public class P4 {
+    public static void main(String[] args) {
+        int salary=15000;
+        // if(salary>10000){
+        //     salary=salary+2000;
+        //     System.out.println(salary);
+        // }
+        // else{
+        //     salary=salary+1000;
+        //     System.out.println(salary);
+        // }
+        if(salary>20000){
+            salary=salary+3000;
+            System.out.println(salary);
+        }
+        else if (salary>10000){
+            salary=salary+2000;
+            System.out.println(salary);
+        }
+        else{
+            salary =salary+1000;
+            System.out.println(salary);
+        }
+    }
+}
