3d9c3dabe8ab1532b497d1f8ca7d80cf82fa6775
sunnyitssimple <sunnyshambhu620201@gmail.com>
2025-07-04 21:25:26 +0530

    video 20 done

---
+++ Topics/Sorting/Cyclicsortingalgo.java
@@ -0,0 +1,26 @@
+
+import java.util.Arrays;
+
+public class Cyclicsortingalgo {
+    public static void main(String[] args) {
+        int[]arr={3,2,4,5,1};
+        cyclicsorting(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void cyclicsorting(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+    }
+     static void swap(int[]arr,int first,int second){
+        int temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+    }
+}
