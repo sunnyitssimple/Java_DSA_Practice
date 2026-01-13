99be0048b9a350d1caff59f770ad060c0313b9f1
sunnyitssimple <sunnyshambhu620201@gmail.com>
2026-01-13 19:36:15 +0530

    recursion lecture binary search done

---
+++ Topics/Recurrsion/binarysearchbyrecursion.java
@@ -0,0 +1,6 @@
+public class binarysearchbyrecursion {
+    public static void main(String[] args) {
+        
+    }
+    
+}
--- Topics/Recurrsion/fibonachhiii.java
+++ Topics/Recurrsion/fibonachhiii.java
@@ -1,6 +1,6 @@
 public class fibonachhiii {
     public static void main(String[] args) {
-        System.out.println(fibo(6));
+        System.out.println(fibo(50));
     }
     static int fibo(int n){
         if(n<2){
