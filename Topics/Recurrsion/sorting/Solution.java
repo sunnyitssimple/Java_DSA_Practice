5a54bc75e677a18f570d0093c18608a0bd16c470
sunnyitssimple <sunnyshambhu620201@gmail.com>
2026-02-10 16:37:14 +0530

    Quiick Sort Lecture Done

---
+++ Topics/Recurrsion/sorting/Solution.java
@@ -0,0 +1,40 @@
+package Topics.Recurrsion.sorting;
+
+import java.util.Arrays;
+
+public class Sorti {
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        // Pointers for nums1, nums2, and the end of the combined space
+        int i = m - 1; 
+        int j = n - 1; 
+        int k = m + n - 1; 
+
+        // Merge from the back to the front to stay "in-place"
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        // Test Case
+        int[] nums1 = {1, 2, 3, 0, 0, 0}; // m=3 valid elements, total size=6
+        int m = 3;
+        int[] nums2 = {2, 5, 6};         // n=3 valid elements
+        int n = 3;
+
+        System.out.println("Before merging: " + Arrays.toString(nums1));
+        
+        // Call the method
+        sol.merge(nums1, m, nums2, n);
+
+        System.out.println("After merging:  " + Arrays.toString(nums1));
+    }
+}
+
